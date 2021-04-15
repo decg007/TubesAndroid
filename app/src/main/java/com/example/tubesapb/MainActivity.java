ec3dea8261af27fa8dd2a8dbe521b70318a96f25
Donald Gracie <donaldgracie@gmail.com>
2021-04-16 06:46:00 +0700

    alrite

--- app/src/main/java/com/example/tubesapb/MainActivity.java
+++ app/src/main/java/com/example/tubesapb/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example, menu);
+        return true;
+    }
+
     public void sendMessage(View view) {
         Intent intent = new Intent(MainActivity.this, pulsa.class);
         startActivity(intent);
